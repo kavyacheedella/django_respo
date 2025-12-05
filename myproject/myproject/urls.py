a02a9f51e80f126e5fec1c5f8cf3f1a78e8dadcf
kavyacheedella <kavyacheedella64@gmail.com>
2025-12-06 01:35:49 +0530

    basicapp

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from basic.views import home,about,contact,services
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),    
+    path('home/',home,name='home'),
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact'),
+    path('services/',services,name='services')
 ]
