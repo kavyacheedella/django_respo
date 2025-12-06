22fbb62bb7a2869cc311559a91cc168e5aa6521f
kavyacheedella <kavyacheedella64@gmail.com>
2025-12-06 11:51:40 +0530

    host added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4)e857#k9_v0il)^xeknqa(14u!4@gx0g=mj2ew(2vfmfpc)%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "second-app-srab.onrender.com"
+]
+
 
 
 # Application definition
