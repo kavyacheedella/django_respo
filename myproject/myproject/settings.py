1f6a9d1e3f1bfcc45b34e52546e22aa760dad748
kavyacheedella <kavyacheedella64@gmail.com>
2025-12-06 11:48:59 +0530

    host added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4)e857#k9_v0il)^xeknqa(14u!4@gx0g=mj2ew(2vfmfpc)%!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["second-app-srab.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
